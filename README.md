e6fd50b128188db51212caa9e38d7722790c330d
Grégory <gregory@galadrim.fr>
2025-05-27 10:22:16 +0200

    chore: added steps

--- README.md
+++ README.md
@@ -42,3 +42,24 @@ echo 'VITE_API_BASE_URL=<your-backend-ip>'
 ```sh
 pnpm run dev
 ```
+
+## The main goal
+
+From here, your main goal is to make the chat working with the backend.
+
+The only file you need to modify is `src/hooks/contexts/websocket.context.tsx`.
+
+The boilerplate for the websocket context is already there, you just need to implement the `send` and `subscribe` functions.
+Which means you will first need to implement the websocket connection.
+
+You might need to see how your implementation will be used, you can refer to `src/hooks/contexts/messages.context.tsx`.
+
+### TL;DR
+
+In `src/hooks/contexts/websocket.context.tsx`:
+
+1. Implement the websocket connection
+2. Implement the websocket send message
+3. Implement the websocket subscribe
+
+Your implementation will be used in `src/hooks/contexts/messages.context.tsx`.
--- src/hooks/contexts/websocket.context.tsx
+++ src/hooks/contexts/websocket.context.tsx
@@ -1,34 +1,13 @@
 import { createContext, useContext, FC, ReactNode } from "react";
 
-type WebSocketContextType = {
-  subscribe: <T extends object>(
-    event: string,
-    handler: (data: T) => void,
-  ) => (() => void) | undefined;
-  send: <T extends object>(
-    event: string,
-    data: unknown,
-    callback?: (response: T) => void,
-  ) => void;
-  isConnected: boolean;
-};
-
-const WebSocketContext = createContext<WebSocketContextType | null>(null);
-
-export const useWebSocket = () => {
-  const ctx = useContext(WebSocketContext);
-  if (ctx === null) {
-    throw new Error("useWebSocket should be used within a WebSocketContext");
-  }
-  return ctx;
-};
-
 export const WebSocketProvider: FC<{ children?: ReactNode; url: string }> = ({
   children,
   url,
 }) => {
+  // TODO: 1. Implement websocket connection
   console.debug(`Connecting to ${url}`);
 
+  // TODO: 2. Implement websocket send message
   const send = <T extends object>(
     event: string,
     data: unknown,
@@ -37,6 +16,7 @@ export const WebSocketProvider: FC<{ children?: ReactNode; url: string }> = ({
     console.debug(`Sending event: ${event} with data: ${data}`);
   };
 
+  // TODO: 3. Implement websocket subscribe
   const subscribe = <T extends object>(
     event: string,
     _handler: (data: T) => void,
@@ -59,4 +39,27 @@ export const WebSocketProvider: FC<{ children?: ReactNode; url: string }> = ({
       {children}
     </WebSocketContext.Provider>
   );
+};
+
+type WebSocketContextType = {
+  subscribe: <T extends object>(
+    event: string,
+    handler: (data: T) => void,
+  ) => (() => void) | undefined;
+  send: <T extends object>(
+    event: string,
+    data: unknown,
+    callback?: (response: T) => void,
+  ) => void;
+  isConnected: boolean;
+};
+
+const WebSocketContext = createContext<WebSocketContextType | null>(null);
+
+export const useWebSocket = () => {
+  const ctx = useContext(WebSocketContext);
+  if (ctx === null) {
+    throw new Error("useWebSocket should be used within a WebSocketContext");
+  }
+  return ctx;
 };
